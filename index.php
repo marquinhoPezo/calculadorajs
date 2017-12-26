9d13ba3fd9d4bfbad650f8fa3d51e5a67d4d663a
= <marq_253@hotmail.com>
2017-12-25 20:41:14 -0500

    empesando a agregar js y css vacios

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
 	<title>Document</title>
 </head>
 <body>
-	
+	<table>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>7</td>
+		</tr>
+	</table>
 </body>
 </html>
