e629031f695c69a42df1520f3cd9eabf6e6b2337
= <marq_253@hotmail.com>
2017-12-26 01:47:08 -0500

    ultimo avance 26_12_2017_hora_01_47

--- index.php
+++ index.php
@@ -6,16 +6,50 @@
 	<title>Document</title>
 </head>
 <body>
-	<table>
-		<tr>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
-			<td>5</td>
-			<td>6</td>
-			<td>7</td>
-		</tr>
-	</table>
+	
+		<table cellspacing="10">
+		
+		<!-- tr -> Filas -->
+		<!-- td -> columnas o celdas  -->
+			<tr id="tr1">
+				<p><td colspan="6" class ="td1"><input type="text" /> 0</td></p>	
+			</tr>
+			<tr id="tr2">
+				<td colspan="2" class="td2"><input type="button" value="RETR"/></td>
+				<td colspan="2" class="td2"><input type="button" value="CE"/></td>
+				<td colspan="2" class="td2"><input type="button" value="C"/></td>
+				
+			</tr>
+			<tr id="tr3" >
+				<td class="td3"><input type="button"  value="7"/></td>
+				<td class="td3"><input type="button" value="8"/></td>
+				<td class="td3"><input type="button" value="9"/></td>
+				<td class="td3"><input type="button" value="/"/></td>
+				<td colspan="2" class="td3"><input type="button" value="RAIZ"/></td>
+			</tr>
+			<tr id="tr4">
+				<td class="td4"><input type="button" value="6"/></td>
+				<td class="td4"><input type="button" value="5"/></td>
+				<td class="td4"><input type="button" value="4"/></td>
+				<td class="td4"><input type="button" value="*"/></td>
+				<td colspan="2" class="td4"><input type="button" value="%"/></td>
+			</tr>
+			<tr id="tr5">
+				<td class="td5"><input type="button" value="3"/></td>
+				<td class="td5"><input type="button" value="2"/></td>
+				<td class="td5"><input type="button" value="1"/></td>
+				<td class="td5"><input type="button" value="-"/></td>
+				<td colspan="2" class="td5"><input type="button" value="1/x"/></td>
+			</tr>
+			<tr id="tr6">
+				<td class="td6"><input type="button" value="0"/></td>
+				<td class="td6"><input type="button" value="+/-"/></td>
+				<td class="td6"><input type="button" value="."/></td>
+				<td class="td6"><input type="button" value="+"/></td>
+				<td colspan="2" class="td6"><input type="button" value="="/></td>
+			</tr>
+			
+		</table>
+	
 </body>
 </html>
